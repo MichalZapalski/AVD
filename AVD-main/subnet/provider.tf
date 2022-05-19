d89040cd5f7f7917879f8ccbc55c0b374c9defe9
Smokecheeba <zapalskimichal2@gmail.com>
2022-05-19 17:18:27 +0200

    sda

--- AVD-main/subnet/provider.tf
+++ AVD-main/subnet/provider.tf
@@ -13,7 +13,7 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
-  subscription_id = "1e5d5a77-5bb8-493e-9a80-87271fc0c024"
+  subscription_id = "7474e679-4ce3-4d84-afd4-61e9e1b650bf"
   features {
     
   }
@@ -23,7 +23,7 @@ provider "azurerm" {
   # Configuration options
 #  features {   
 #  }
-#  subscription_id = "1e5d5a77-5bb8-493e-9a80-87271fc0c024"
+#  subscription_id = "7474e679-4ce3-4d84-afd4-61e9e1b650bf"
 #  alias = "B"
 #}
 
