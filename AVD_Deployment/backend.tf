e6aaa30fad6cc7803296087c5c52a8b06667a3f7
MichalZapalski <105287826+MichalZapalski@users.noreply.github.com>
2024-02-21 14:27:43 +0100

    Update backend.tf

--- AVD_Deployment/backend.tf
+++ AVD_Deployment/backend.tf
@@ -1,11 +1,11 @@
 /*
 terraform {
   backend "azurerm" {
-    subscription_id      = "5ddf68c5-74a1-4a71-9092-0040ff11204e"
-    resource_group_name  = "rg-05896-d-004"
-    storage_account_name = "sa05896d001"
+    subscription_id      = "id of the subscription"
+    resource_group_name  = "resource group name"
+    storage_account_name = "storage account name"
     container_name       = "tfbackend"
-    key                  = "avd_deployment.tfstate"
+    key                  = "tfstate file key"
     #use_oidc              = true
   }
 }
@@ -13,8 +13,8 @@ terraform {
 terraform {
   backend "azurerm" {
     subscription_id      = "f46c3ccd-e389-4887-a964-baa05ec5b242"
-    resource_group_name  = "rg-avd-infra"
-    storage_account_name = "avdstoragewesteurope"
+    resource_group_name  = "name of the resource group"
+    storage_account_name = "name of the storage account name"
     container_name       = "tfbackend"
     key                  = "avd_deployment.tfstate"
     #use_oidc              = true
