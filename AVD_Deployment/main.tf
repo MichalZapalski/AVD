837b0a699ea1b6e279816e9ebf40e8c30d4909c1
MichalZapalski <105287826+MichalZapalski@users.noreply.github.com>
2024-02-21 14:32:53 +0100

    Update main.tf

--- AVD_Deployment/main.tf
+++ AVD_Deployment/main.tf
@@ -1,35 +1,35 @@
 module "avdworkspace" {
   source               = "../AVD_workspace"
-  wrkspc_name          = "TeamCenter_Dev"
-  wrkspc_rg_name       = "rg-stella-test"
-  wrkspc_description   = "Dev environment"
-  wrkspc_friendly_name = "Dev environment"
+  wrkspc_name          = "name of workspace"
+  wrkspc_rg_name       = "workspace resource group name"
+  wrkspc_description   = "workspace description"
+  wrkspc_friendly_name = "workspace friendly name"
   deploy_location      = "west europe"
   tags_name = {
-    "Environment"    = "Dev"
-    "appid"          = "05896"
+    "Environment"    = "environment "
+    "appid"          = "app id"
     "application_id" = "avd"
   }
 }
 
 module "ag_hp" {
   source          = "../AVD_hp_ag"
-  rg_name         = "rg-stella-test"
+  rg_name         = "resource group name"
   deploy_location = "west europe"
-  hp_descript     = "hostpul"
-  hp_name         = "myavdhostpool"
-  hp_fr_name      = "anyhostpool"
+  hp_descript     = "host pool description"
+  hp_name         = "host pool name"
+  hp_fr_name      = "host pool friendly name"
   hp_type         = "Pooled"
   rdp_prop        = "drivestoredirect:s:*;audiomode:i:0;videoplaybackmode:i:1;redirectclipboard:i:1;redirectprinters:i:1;devicestoredirect:s:*;redirectcomports:i:1;redirectsmartcards:i:1;usbdevicestoredirect:s:*;enablecredsspsupport:i:1;redirectwebauthn:i:1;use multimon:i:1"
   lb_type         = "DepthFirst"
-  appgr_descript  = "aplikajszongroup"
-  appgr_disp_name = "Default desktop"
-  appgr_fr_name   = "anyappgr"
-  appgr_name      = "avdappgroup"
+  appgr_descript  = "Application group description"
+  appgr_disp_name = "Application group display name"
+  appgr_fr_name   = "Application Group friendly name"
+  appgr_name      = "Application Group Name"
   appgr_type      = "Desktop"
   workspace_id    = module.avdworkspace.workspaceid
   tags_name = {
-    "Environment"    = "Dev"
+    "Environment"    = "Name of the environment"
     "appid"          = "05896"
     "application_id" = "avd"
   }
@@ -50,20 +50,20 @@ module "diag_sett" {
 
 module "AVD_machines" {
   source                      = "../AVD_machines"
-  rg_name                     = "rg-stella-test"
+  rg_name                     = "resource group name"
   deploy_location             = "west europe"
   rdsh_count                  = "1"
-  prefix                      = "VDItest"
+  prefix                      = "name of virtual machine"
   vm_size                     = "Standard_D2s_v4"
   existingVnetName            = "vnet-avd"
-  existingSubnetName          = "snet-avd-pier"
+  existingSubnetName          = "name of subnet"
   existingWVDAppGroupName     = module.ag_hp.appgrnm
   existingWVDHostPoolName     = module.ag_hp.hpnm
   existingWVDWorkspaceName    = module.avdworkspace.workspacenm
   drainmode                   = "no"
   createWorkspaceAppGroupAsso = "No"
-  SubscriptionId              = "f46c3ccd-e389-4887-a964-baa05ec5b242"
-  kv_name                     = "kv-avd-plumeria"
+  SubscriptionId              = "ID of the subscription"
+  kv_name                     = "name of keyvault"
   lg_wrkspc_name              = "la-avd"
   lg_rg_name                  = "rg-la-avd"
   vnetRg                      = "rg-avd-infra"
@@ -72,7 +72,7 @@ module "AVD_machines" {
     module.ag_hp
   ]
   tags_name = {
-    "Environment"    = "Dev"
+    "Environment"    = "name of environment"
     "appid"          = "05896"
     "application_id" = "avd"
   }
