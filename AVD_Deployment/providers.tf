6f4aa8ffaa2a0b968d5a10895fccb778c90612ac
MichalZapalski <105287826+MichalZapalski@users.noreply.github.com>
2024-02-21 14:33:47 +0100

    Update providers.tf

--- AVD_Deployment/providers.tf
+++ AVD_Deployment/providers.tf
@@ -16,13 +16,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "5ddf68c5-74a1-4a71-9092-0040ff11204e"
+  subscription_id = "subscription ID"
   #use_oidc = true
   skip_provider_registration = true
 }
 provider "azurerm" {
   features {}
-  subscription_id            = "6bb0017a-46b6-494b-b79e-1881466cf7c9"
+  subscription_id            = "subscription ID"
   #use_oidc                   = true
   alias                      = "la-provider"
   skip_provider_registration = true
@@ -45,7 +45,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "f46c3ccd-e389-4887-a964-baa05ec5b242"
+  subscription_id = "subscription ID"
   #use_oidc = true
   skip_provider_registration = true
 }
